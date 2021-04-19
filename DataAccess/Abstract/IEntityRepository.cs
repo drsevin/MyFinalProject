4f2e2a8b05616861c73bdba0abf0f762c567936f
Şevin Sena Dere <sevin.dere@ogr.sakarya.edu.tr>
2021-04-19 13:55:03 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint - kısıtlama
+    //class: reefrans tip olabilir
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir 
+    // new() : new lenebilir olmalı (IEntity interface olduğu için newlenemez bu yüzden sadece IEntity implemente eden bir nesne yazılabilir)
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        // Ürünleri kategoriye , fiyata göre listelemeyi sağlıyor
+        List<T> GetAll(Expression<Func< T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+        //Bu ürünleri kategoriye göre filtrele
+        //List<T> GetAllByCategory(int categoryId);
+
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //IEntityRepository i Product için yapılandırma anlamına gelir
+    public interface IProductDal:IEntityRepository<Product>
+    {
+        
+    }
+}
